f43c273747e9f0b94f48cc3188c16d96f5d2a9fa
EnioHenrique10 <126336176+EnioHenrique10@users.noreply.github.com>
2023-12-20 17:58:08 -0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import {Text, View } from 'react-native';
+import Home from './src/screens/Home/Home';
+
+export default function App() {
+  return (
+    
+      <Home />
+      
+   
+  );
+}
+
+
